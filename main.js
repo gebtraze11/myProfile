dd3e4bb3f286ef2efa7aca1bff1baa2a38ca1fdc
KaleyMoro <csrijan10@gmail.com>
2019-09-05 01:39:21 -0700

    fixed animation bug on scroll

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
-$("#hoverAni img").hover(function(){
-    
-    $(this).addClass("hoverAni")
-    $(this).css("animation-play-state","initial")
+$("#hoverAni img").hover(function () {
 
-}, function(){
-     $(this).css("animation-play-state","paused")
+    $(this).addClass("hoverAni")
+    $(this).css("animation-play-state", "initial")
+
+}, function () {
+    $(this).removeClass("hoverAni")
+
+
+    if ($(this).hasClass('fadeInRight')) {
+        $(this).removeClass('fadeInRight')
+    } else {
+        $(this).removeClass('fadeInLeft')
+    };
+
 });
 
-$(".page-content").on( 'scroll', function(event){
+$(".page-content").on('scroll', function (event) {
     console.log('Event Fired');
     var hT = $('#skills').offset().top,
-    hH = $('#skills').outerHeight(),
-    wH = $(window).height(),
-    wS = $(this).scrollTop();
-if (wS > (hT+hH-wH)){
-   // console.log('on the view!');
-    $('#hoverAni img').css("animation-play-state","initial")
-}
+        hH = $('#skills').outerHeight(),
+        wH = $(window).height(),
+        wS = $(this).scrollTop();
+    if (wS > (hT + hH - wH)) {
+        // console.log('on the view!');
+
+
+        $('#hoverAni img').css("animation-play-state", "running")
+    }
     //console.log(event.target)
 
- });
+});
+
+
+$('#portfolio img').hover(function(){
+
+}, function(){
+
+}); 

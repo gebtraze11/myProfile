6ce07a0a532e4c1b63652b57b7abfc3ed7cd74dd
KaleyMoro <csrijan10@gmail.com>
2019-09-04 21:01:54 -0700

    shows animation when scroled to the content

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 $("#hoverAni img").hover(function(){
+    
     $(this).addClass("hoverAni")
-    $(this).css("animation-play-state","running")
+    $(this).css("animation-play-state","initial")
+
 }, function(){
      $(this).css("animation-play-state","paused")
 });
 
+$(".page-content").on( 'scroll', function(event){
+    console.log('Event Fired');
+    var hT = $('#skills').offset().top,
+    hH = $('#skills').outerHeight(),
+    wH = $(window).height(),
+    wS = $(this).scrollTop();
+if (wS > (hT+hH-wH)){
+   // console.log('on the view!');
+    $('#hoverAni img').css("animation-play-state","initial")
+}
+    //console.log(event.target)
+
+ });
